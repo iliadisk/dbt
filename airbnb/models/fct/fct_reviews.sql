1c16b4ce37d1367f084c4b4bc44d7241f5931bca
Constantine Eliades <iliadisk.the@gmail.com>
2026-02-15 18:13:44 +0200

    Refactor code structure for improved readability and maintainability

---
+++ airbnb/models/fct/fct_reviews.sql
@@ -0,0 +1,19 @@
+{{
+    config(
+    materialized = 'incremental',
+    on_schema_change = 'fail'
+    )
+}}
+WITH src_Reviews AS 
+(
+    SELECT * FROM {{ ref('src_reviews') }}
+)
+    SELECT
+        {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} AS review_id,
+        *    
+    FROM 
+        src_Reviews
+    WHERE review_text IS NOT NULL
+{% if is_incremental() %}
+    AND review_date > (SELECT MAX(review_date) FROM {{ this }})
+{% endif %}
